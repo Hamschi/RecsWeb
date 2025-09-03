add8ed798d11e40486abc811445f6de6920b1ce5
Luu <linhson.luu@stud.h-da.de>
2025-09-04 01:06:29 +0200

    switching to relative paths again

--- assets/scripts.js
+++ assets/scripts.js
@@ -44,8 +44,8 @@ function initPartials() {
       .catch(err => warn(`Fehler beim Laden von ${url}:`, err));
   };
 
-  loadPartial('site-navbar', '/partials/navbar.html');
-  loadPartial('site-footer', '/partials/footer.html');
+  loadPartial('site-navbar', '../../partials/navbar.html');
+  loadPartial('site-footer', '../../partials/footer.html');
 }
 
 // =====================================================
